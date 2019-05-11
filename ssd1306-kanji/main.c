e13cd9e17002103675ce4023113769a811a6b564
nopnop2002 <hau26280@tree.odn.ne.jp>
2019-05-11 15:16:31 +0900

    Update main.c

--- ssd1306-kanji/main.c
+++ ssd1306-kanji/main.c
@@ -60,7 +60,7 @@ const font_info_t *font = NULL; // current font
 
 #define MYBUFSZ 128
 
-static void SPIFFS_Directory() {
+static void printDirectory() {
     spiffs_DIR d;
     struct spiffs_dirent e;
     struct spiffs_dirent *pe = &e;
@@ -84,8 +84,8 @@ void ssd1306_invert_frame(uint8_t * src, uint8_t * dst) {
 
 
 int ssd1306_draw_font(const ssd1306_t *dev, uint8_t *fb, uint8_t x, uint8_t y, 
-	uint8_t *fonts, uint8_t pw, uint8_t ph,
-        ssd1306_color_t foreground, ssd1306_color_t background)
+    uint8_t *fonts, uint8_t pw, uint8_t ph,
+    ssd1306_color_t foreground, ssd1306_color_t background)
 {
     uint8_t i, j;
     int err;
@@ -119,11 +119,37 @@ int ssd1306_draw_font(const ssd1306_t *dev, uint8_t *fb, uint8_t x, uint8_t y,
     return pw;
 } 
 
+void ssd1306_draw_sjis(const ssd1306_t *dev, uint8_t *fb, FontxFile *fx, 
+     int xs, int ys, uint16_t *sjis, int sjisLen,
+     ssd1306_color_t foreground, ssd1306_color_t background)
+{
+    uint8_t fonts[128];
+    uint8_t pw;
+    uint8_t ph;
+    bool rc;
+    int x = xs;
+    int y = ys;
+
+    for (int i=0;i<sjisLen;i++) {
+      printf("sjis[%d]=0x%x\n",i,sjis[i]);
+      rc = GetFontx(fx, sjis[i], fonts, &pw, &ph); // SJIS -> Fontパターン
+      printf("rc=%d pw=%d ph=%d\n",rc,pw,ph);
+      if (!rc) continue;
+      ShowFont(fonts, pw, ph);
+
+      ssd1306_draw_font(dev, fb, x, y, fonts, pw, ph, foreground, background);
+      x = x + pw;
+
+      //Font2Bitmap(fonts, bitmap, pw, ph, 0);
+      //ShowBitmap(bitmap, pw, ph);
+    }
+}
+
 static void ssd1306_task(void *pvParameters)
 {
     TickType_t nowTick;
     printf("%s: Started user interface task\n", __FUNCTION__);
-    SPIFFS_Directory();
+    printDirectory();
     font = font_builtin_fonts[7];
 
     // Open UTF8 to SJIS table
@@ -135,57 +161,34 @@ static void ssd1306_task(void *pvParameters)
       }
     }
 
-    ssd1306_set_whole_display_lighting(&dev, true);
-    vTaskDelay(SECOND);
     ssd1306_set_whole_display_lighting(&dev, false);
     vTaskDelay(SECOND);
-    ssd1306_fill_rectangle(&dev, buffer, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT/2, OLED_COLOR_BLACK);
-
-    char str_utf[MYBUFSZ+1];
-    uint16_t sjis[128];
-    int sjisLen;
-
-    uint8_t fonts[128];
-    //uint8_t bitmap[128];
-    uint8_t pw;
-    uint8_t ph;
-    int x, y;
-    int i;
 
     /* 変換元文字列を作成（このソースはUTF-8で書かれている）*/
+    char str_utf[MYBUFSZ+1];
     strcpy(str_utf, "漢字TESTｶﾝｼﾞ");
     printf("%d\n", strlen(str_utf));
 
     /* 文字コード変換(UTF-8->SJIS) */
+    uint16_t sjis[128];
+    int sjisLen;
     sjisLen = String2SJIS(fd, (unsigned char *)str_utf, strlen(str_utf), sjis, 128);
+
     /* フォントファイルの指定(お好みで) */
     FontxFile fx[2];
     InitFontx(fx,"ILGH16XB.FNT","ILGZ16XB.FNT"); // 16Dot Gothic
     //InitFontx(fx,"ILMH16XB.FNT","ILMZ16XB.FNT"); // 16Dot Mincyo
     //InitFontx(fx,"ILGH24XB.FNT","ILGZ24XB.FNT"); // 24Dot Gothic
 
-    bool rc;
-    x = 0;
-    y = 0;
-    for (i=0;i<sjisLen;i++) {
-      printf("sjis[%d]=0x%x\n",i,sjis[i]);
-      rc = GetFontx(fx, sjis[i], fonts, &pw, &ph); // SJIS -> Fontパターン
-      printf("rc=%d pw=%d ph=%d\n",rc,pw,ph);
-      if (!rc) continue;
-      ShowFont(fonts, pw, ph);
-
-      ssd1306_draw_font(&dev, buffer, x, y, fonts, pw, ph, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-      x = x + pw;
-
-      //Font2Bitmap(fonts, bitmap, pw, ph, 0);
-      //ShowBitmap(bitmap, pw, ph);
-    }
+    /* SJIS表示 */
+    ssd1306_draw_sjis(&dev, buffer, fx, 0, 0, sjis, sjisLen, OLED_COLOR_WHITE, OLED_COLOR_TRANSPARENT);
 
     char text[20];
     while (1) {
         nowTick = xTaskGetTickCount();
+        ssd1306_fill_rectangle(&dev, buffer, 0, DISPLAY_HEIGHT/2, DISPLAY_WIDTH, DISPLAY_HEIGHT/2, OLED_COLOR_BLACK);
         sprintf(text, "Tick: %d   ", nowTick);
-        ssd1306_draw_string(&dev, buffer, font_builtin_fonts[DEFAULT_FONT], 0, 45, text, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+        ssd1306_draw_string(&dev, buffer, font_builtin_fonts[DEFAULT_FONT], 0, 45, text, OLED_COLOR_WHITE, OLED_COLOR_TRANSPARENT);
 
         ssd1306_invert_frame(buffer, invert_buffer);
         if (ssd1306_load_frame_buffer(&dev, invert_buffer)) {
